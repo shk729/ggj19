eba5a654f1eeb8d71811d2a524948215fe225add
shk729 <takeshark729@gmail.com>
2019-01-27 14:51:13 +0300

    Spawn triggers

---
+++ New Unity Project/Assets/SpawnPoint.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawCube(transform.position, Vector3.one);
+
+    }
+}
---
+++ New Unity Project/Assets/TriggerPoint.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerPoint : MonoBehaviour
+{
+
+    public string victoryScene;
+    public string victoryName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Component[] spawners = this.transform.parent.GetComponentsInChildren<SpawnPoint>();
+        var el = Mathf.FloorToInt( Random.Range(0f, spawners.Length) );
+        this.transform.position = spawners[el].transform.position;
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == victoryName) UnityEngine.SceneManagement.SceneManager.LoadScene(victoryScene);
+    }
+}

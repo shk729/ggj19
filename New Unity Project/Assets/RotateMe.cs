2b870cc60da7795d7ba2c5e03ca0c77fe1e37e0d
shk729 <takeshark729@gmail.com>
2019-01-27 17:58:59 +0300

    rotator prefab

---
+++ New Unity Project/Assets/RotateMe.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateMe : MonoBehaviour
+{
+    public float force = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //this.GetComponent<Rigidbody>().(transform.forward * force);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        var rot = transform.rotation;
+        Vector3 vec3 = rot.eulerAngles;
+        vec3.z += force;
+        transform.rotation = Quaternion.Euler(vec3);
+    }
+}

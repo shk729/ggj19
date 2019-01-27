0b75ee6c88537cfb840b0acfb0cded18fb5bbd04
Godmesser <paterlupus@gmail.com>
2019-01-27 14:41:44 +0300

    Вин скрины работают и возвращают в мейн меню

--- New Unity Project/Assets/MainMenu.cs
+++ New Unity Project/Assets/MainMenu.cs
@@ -16,4 +16,9 @@ public class MainMenu : MonoBehaviour
         Debug.Log("Quit");
         Application.Quit();
     }
+
+    public void BackToMenu ()
+    {
+        SceneManager.LoadScene("Menu");
+    }
 }
